339b95c38e00edb528a36d1f9073327172a9f128
SofiaCaterini <63949976+SofiaCaterini@users.noreply.github.com>
2020-12-30 19:57:37 +0100

    Aggiunta toolbar e menu non perfettamente funzionante

--- app/src/main/java/it/polito/kgame/HomeActivity.kt
+++ app/src/main/java/it/polito/kgame/HomeActivity.kt
@@ -2,10 +2,33 @@ package it.polito.kgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.home_layout.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_layout)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
+

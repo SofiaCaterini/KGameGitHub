6287b441e704cf5395fbab0f9e0e9f927b42812c
SofiaCaterini <63949976+SofiaCaterini@users.noreply.github.com>
2021-02-23 16:22:02 +0100

    layout

--- app/src/main/java/it/polito/kgame/ui/account/AccountFragment.kt
+++ app/src/main/java/it/polito/kgame/ui/account/AccountFragment.kt
@@ -1,7 +1,9 @@
 package it.polito.kgame.ui.account
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -22,6 +24,13 @@ class AccountFragment : Fragment(R.layout.fragment_account) {
         rv.layoutManager= LinearLayoutManager(requireContext())
         rv.adapter = adapter
 
+        fun View.hideKeyboard(){
+            val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE ) as InputMethodManager
+            imm.hideSoftInputFromWindow(windowToken, 0)
+        }
+        view.setOnClickListener { view.hideKeyboard() }
 
     }
+
+
 }

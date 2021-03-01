1334e81ae80b77a621d5c1321fe892c7986ae2d6
SofiaCaterini <63949976+SofiaCaterini@users.noreply.github.com>
2021-03-01 11:18:39 +0100

    keyboard
    Prova togliere keyboard

--- app/src/main/java/it/polito/kgame/ui/account/AccountFragment.kt
+++ app/src/main/java/it/polito/kgame/ui/account/AccountFragment.kt
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.navigation.NavigationView
 import it.polito.kgame.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.fragment_account.*
 
@@ -23,13 +25,15 @@ class AccountFragment : Fragment(R.layout.fragment_account) {
         viewModel.data.observe(viewLifecycleOwner, Observer { data-> adapter.setData(data) })
         rv.layoutManager= LinearLayoutManager(requireContext())
         rv.adapter = adapter
-
+        val tView: View = requireActivity().toolbar
+        val nView: View = requireActivity().nav_view
         fun View.hideKeyboard(){
             val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE ) as InputMethodManager
             imm.hideSoftInputFromWindow(windowToken, 0)
         }
         view.setOnClickListener { view.hideKeyboard() }
-
+        tView.setOnClickListener { view.hideKeyboard()  }
+        nView.setOnClickListener { view.hideKeyboard()  }
     }
 
 
